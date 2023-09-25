5ef5ce024cc0e1b7f8aca445d483cd116ca4def7
Marc Chacon <marcchacon1@outlook.com>
2023-09-25 09:31:11 -0500

    added function count(flavors0

--- froyo-order.js
+++ froyo-order.js
@@ -2,3 +2,13 @@ const order = prompt(
     "Please enter a list of Frozen Yogurt flavors, separate each flavor with commas."
     "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 )
+
+const usrFlavorArray = order.split(",");
+
+function count(flavors){
+    const froyo = {}
+    for (const flavor of usrFlavorArray){
+        froyo[flavor] = froyo[flavor] +1 || 1;
+    }
+    return froyo;
+}
